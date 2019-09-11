9ba193b6b46de002b7624aafddfcf4587c37e9fc
SaulGarza <asul5000@gmail.com>
2019-09-11 13:41:57 -0400

    removing logs for each request

--- server/app.ts
+++ server/app.ts
@@ -10,14 +10,14 @@ import express = require('express')
 const app = express()
 
 app.use(function(req, res, next) {
-  console.log(req.protocol, typeof req.protocol)
+  // console.log(req.protocol, typeof req.protocol)
   if (req.protocol !== 'https' && req.headers["X-Forwarded-Proto"] === 'https') {
-    sidewalk.detour('Redirecting Unsecure Connection to HTTP')
+    // sidewalk.detour('Redirecting Unsecure Connection to HTTP')
     let redirect = 'https://' + req.hostname + ':' + (process.env.PORT || 8080) + req.originalUrl
     if(process.env.NODE_ENV === 'production') redirect = 'https://' + req.hostname + req.originalUrl
     res.redirect(301, redirect);
   } else {
-    sidewalk.emphasize('Established Secure Connection to HTTPS')
+    // sidewalk.emphasize('Established Secure Connection to HTTPS')
     return next();
   }
 });
