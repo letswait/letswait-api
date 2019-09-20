73637101bd33026587f3c970513722bbece0ecb3
SaulGarza <asul5000@gmail.com>
2019-09-20 13:17:02 -0400

    backing up and stashing current data. spring cleaning in preparation for macOS Catalina

---
+++ server/types/expressOverride.d.ts
@@ -0,0 +1,13 @@
+
+
+// import express = require('express');
+
+// declare namespace e {
+//   export interface MethodOverrideOptions {
+//       methods: string[];
+//   }
+// }
+
+// declare function e(getter?: string | ((req: express.Request, res: express.Response) => string), options?: e.MethodOverrideOptions): express.RequestHandler;
+
+// export = e;
